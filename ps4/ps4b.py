b74a220e843e046715f69a68dac4e846c427bae6
Victor Perrin <victor.perrin22@gmail.com>
2020-02-01 19:04:32 +0100

    Work on PS4

--- ps4/ps4b.py
+++ ps4/ps4b.py
@@ -8,11 +8,11 @@ import string
 ### HELPER CODE ###
 def load_words(file_name):
     '''
-    file_name (string): the name of the file containing 
-    the list of words to load    
-    
+    file_name (string): the name of the file containing
+    the list of words to load
+
     Returns: a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     '''
@@ -33,7 +33,7 @@ def is_word(word_list, word):
 
     word_list (list): list of words in the dictionary.
     word (string): a possible word.
-    
+
     Returns: True if word is in word_list, False otherwise
 
     Example:
@@ -63,31 +63,32 @@ class Message(object):
     def __init__(self, text):
         '''
         Initializes a Message object
-                
+
         text (string): the message's text
 
         a Message object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        self.message_text = text
+        self.valid_words  = load_words(WORDLIST_FILENAME)
 
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
+
         Returns: self.message_text
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text
 
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class.
         This helps you avoid accidentally mutating class attributes.
-        
+
         Returns: a COPY of self.valid_words
         '''
-        pass #delete this line and replace with your code here
+        return self.valid_words.copy()
 
     def build_shift_dict(self, shift):
         '''
@@ -95,35 +96,51 @@ class Message(object):
         The dictionary maps every uppercase and lowercase letter to a
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
-        letters only.        
-        
-        shift (integer): the amount by which to shift every letter of the 
+        letters only.
+
+        shift (integer): the amount by which to shift every letter of the
         alphabet. 0 <= shift < 26
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
         '''
-        pass #delete this line and replace with your code here
+        assert(0 <= shift < 26), "shift not in [0,26]: %d" % shift
+        cipher = {}
+        for c in string.ascii_lowercase:
+            if ord(c) + shift > ord("z"):
+                cipher[c] = chr(ord(c) + shift - ord("z") + ord("a") - 1)
+            else:
+                cipher[c] = chr(ord(c) + shift)
+            cipher[c.upper()] = cipher[c].upper()
+        return cipher
 
     def apply_shift(self, shift):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
         Creates a new string that is self.message_text shifted down the
-        alphabet by some number of characters determined by the input shift        
-        
+        alphabet by some number of characters determined by the input shift
+
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
 
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
-        pass #delete this line and replace with your code here
+        cipher = self.build_shift_dict(shift)
+
+        cipher_text = ""
+        for c in self.message_text:
+            if c in string.ascii_lowercase + string.ascii_uppercase:
+                cipher_text += cipher[c]
+            else:
+                cipher_text += c
+        return cipher_text
 
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
         '''
-        Initializes a PlaintextMessage object        
-        
+        Initializes a PlaintextMessage object
+
         text (string): the message's text
         shift (integer): the shift associated with this message
 
@@ -135,57 +152,62 @@ class PlaintextMessage(Message):
             self.message_text_encrypted (string, created using shift)
 
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encryption_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
 
     def get_shift(self):
         '''
         Used to safely access self.shift outside of the class
-        
+
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encryption_dict(self):
         '''
         Used to safely access a copy self.encryption_dict outside of the class
-        
+
         Returns: a COPY of self.encryption_dict
         '''
-        pass #delete this line and replace with your code here
+        return self.encryption_dict.copy()
 
     def get_message_text_encrypted(self):
         '''
         Used to safely access self.message_text_encrypted outside of the class
-        
+
         Returns: self.message_text_encrypted
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text_encrypted
 
     def change_shift(self, shift):
         '''
-        Changes self.shift of the PlaintextMessage and updates other 
-        attributes determined by shift.        
-        
+        Changes self.shift of the PlaintextMessage and updates other
+        attributes determined by shift.
+
         shift (integer): the new shift that should be associated with this message.
         0 <= shift < 26
 
         Returns: nothing
         '''
-        pass #delete this line and replace with your code here
+        self.shift = shift
+        self.encryption_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
 
 
 class CiphertextMessage(Message):
     def __init__(self, text):
         '''
         Initializes a CiphertextMessage object
-                
+
         text (string): the message's text
 
         a CiphertextMessage object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
 
     def decrypt_message(self):
         '''
@@ -193,32 +215,52 @@ class CiphertextMessage(Message):
         and find the "best" one. We will define "best" as the shift that
         creates the maximum number of real words when we use apply_shift(shift)
         on the message text. If s is the original shift value used to encrypt
-        the message, then we would expect 26 - s to be the best shift value 
+        the message, then we would expect 26 - s to be the best shift value
         for decrypting it.
 
-        Note: if multiple shifts are equally good such that they all create 
-        the maximum number of valid words, you may choose any of those shifts 
+        Note: if multiple shifts are equally good such that they all create
+        the maximum number of valid words, you may choose any of those shifts
         (and their corresponding decrypted messages) to return
 
         Returns: a tuple of the best shift value used to decrypt the message
         and the decrypted message text using that shift value
         '''
-        pass #delete this line and replace with your code here
+        res = {}
+        max_word_shift = 0
+        for s in range(26):
+            res[s] = {}
+            res[s]["text"] = self.apply_shift(25 - s)
+            # Check how many words exits there
+            res[s]["words"] = 0
+            for w in res[s]["text"].split():
+                if is_word(self.valid_words, w):
+                    res[s]["words"] += 1
+            if res[s]["words"] > max_word_shift:
+                max_word_shift = s
+
+        # print(res)
+        return (25 - max_word_shift, res[max_word_shift]["text"])
 
 if __name__ == '__main__':
 
-#    #Example test case (PlaintextMessage)
-#    plaintext = PlaintextMessage('hello', 2)
-#    print('Expected Output: jgnnq')
-#    print('Actual Output:', plaintext.get_message_text_encrypted())
-#
-#    #Example test case (CiphertextMessage)
-#    ciphertext = CiphertextMessage('jgnnq')
-#    print('Expected Output:', (24, 'hello'))
-#    print('Actual Output:', ciphertext.decrypt_message())
+   #Example test case (PlaintextMessage)
+   plaintext = PlaintextMessage('hello', 2)
+   print('Expected Output: jgnnq')
+   print('Actual Output:', plaintext.get_message_text_encrypted())
 
-    #TODO: WRITE YOUR TEST CASES HERE
+   #Example test case (CiphertextMessage)
+   ciphertext = CiphertextMessage('jgnnq')
+   print('Expected Output:', (24, 'hello'))
+   print('Actual Output:', ciphertext.decrypt_message())
 
-    #TODO: best shift value and unencrypted story 
-    
-    pass #delete this line and replace with your code here
+   plaintext = PlaintextMessage('snor', 25)
+   print('Expected Output: rmnq')
+   print('Actual Output:', plaintext.get_message_text_encrypted())
+
+   ciphertext = CiphertextMessage('rmnq')
+   print('Expected Output:', (1, 'snor'))
+   print('Actual Output:', ciphertext.decrypt_message())
+
+   story = get_story_string()
+   ciphertext = CiphertextMessage(story)
+   print(ciphertext.decrypt_message())
--- ps4/ps4c.py
+++ ps4/ps4c.py
@@ -9,15 +9,15 @@ from ps4a import get_permutations
 ### HELPER CODE ###
 def load_words(file_name):
     '''
-    file_name (string): the name of the file containing 
-    the list of words to load    
-    
+    file_name (string): the name of the file containing
+    the list of words to load
+
     Returns: a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     '''
-    
+
     print("Loading word list from file...")
     # inFile: file
     inFile = open(file_name, 'r')
@@ -35,7 +35,7 @@ def is_word(word_list, word):
 
     word_list (list): list of words in the dictionary.
     word (string): a possible word.
-    
+
     Returns: True if word is in word_list, False otherwise
 
     Example:
@@ -63,64 +63,77 @@ class SubMessage(object):
     def __init__(self, text):
         '''
         Initializes a SubMessage object
-                
+
         text (string): the message's text
 
         A SubMessage object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
-    
+        self.message_text = text
+        self.valid_words  = load_words(WORDLIST_FILENAME)
+
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
+
         Returns: self.message_text
         '''
-        pass #delete this line and replace with your code here
+        return self.message_text
 
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class.
         This helps you avoid accidentally mutating class attributes.
-        
+
         Returns: a COPY of self.valid_words
         '''
-        pass #delete this line and replace with your code here
-                
+        return self.valid_words.copy()
+
     def build_transpose_dict(self, vowels_permutation):
         '''
         vowels_permutation (string): a string containing a permutation of vowels (a, e, i, o, u)
-        
+
         Creates a dictionary that can be used to apply a cipher to a letter.
         The dictionary maps every uppercase and lowercase letter to an
-        uppercase and lowercase letter, respectively. Vowels are shuffled 
-        according to vowels_permutation. The first letter in vowels_permutation 
+        uppercase and lowercase letter, respectively. Vowels are shuffled
+        according to vowels_permutation. The first letter in vowels_permutation
         corresponds to a, the second to e, and so on in the order a, e, i, o, u.
-        The consonants remain the same. The dictionary should have 52 
+        The consonants remain the same. The dictionary should have 52
         keys of all the uppercase letters and all the lowercase letters.
 
         Example: When input "eaiuo":
         Mapping is a->e, e->a, i->i, o->u, u->o
         and "Hello World!" maps to "Hallu Wurld!"
 
-        Returns: a dictionary mapping a letter (string) to 
-                 another letter (string). 
+        Returns: a dictionary mapping a letter (string) to
+                 another letter (string).
         '''
-        
-        pass #delete this line and replace with your code here
-    
+
+        cipher = {}
+        for c in string.ascii_lowercase:
+            if c in VOWELS_LOWER:
+                cipher[c] = vowels_permutation[VOWELS_LOWER.index(c)]
+            else:
+                cipher[c] = c
+            cipher[c.upper()] = cipher[c].upper()
+        return cipher
+
     def apply_transpose(self, transpose_dict):
         '''
         transpose_dict (dict): a transpose dictionary
-        
-        Returns: an encrypted version of the message text, based 
+
+        Returns: an encrypted version of the message text, based
         on the dictionary
         '''
-        
-        pass #delete this line and replace with your code here
-        
+        res = ""
+        for c in self.message_text:
+            if c in transpose_dict:
+                res += transpose_dict[c]
+            else:
+                res += c
+        return res
+
 class EncryptedSubMessage(SubMessage):
     def __init__(self, text):
         '''
@@ -132,28 +145,44 @@ class EncryptedSubMessage(SubMessage):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        SubMessage.__init__(self, text)
 
     def decrypt_message(self):
         '''
-        Attempt to decrypt the encrypted message 
-        
+        Attempt to decrypt the encrypted message
+
         Idea is to go through each permutation of the vowels and test it
         on the encrypted message. For each permutation, check how many
         words in the decrypted text are valid English words, and return
         the decrypted message with the most English words.
-        
-        If no good permutations are found (i.e. no permutations result in 
+
+        If no good permutations are found (i.e. no permutations result in
         at least 1 valid word), return the original string. If there are
         multiple permutations that yield the maximum number of words, return any
         one of them.
 
-        Returns: the best decrypted message    
-        
+        Returns: the best decrypted message
+
         Hint: use your function from Part 4A
         '''
-        pass #delete this line and replace with your code here
-    
+        max_word_iter = 0
+        max_words = -1
+        res = []
+        for i, perm in enumerate(get_permutations(VOWELS_LOWER)):
+            res.append(self.apply_transpose(self.build_transpose_dict(perm)))
+            # Check how many words exits there
+            words = 0
+            for w in res[i].split():
+                if is_word(self.valid_words, w):
+                    words += 1
+            if words > max_words:
+                max_words = words
+                max_word_iter = i
+
+        if max_word_iter == -1:
+            return self.message_text
+        else:
+            return res[max_word_iter]
 
 if __name__ == '__main__':
 
@@ -166,5 +195,21 @@ if __name__ == '__main__':
     print("Actual encryption:", message.apply_transpose(enc_dict))
     enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
     print("Decrypted message:", enc_message.decrypt_message())
-     
-    #TODO: WRITE YOUR TEST CASES HERE
+
+    message = SubMessage("Goodbye my good Lord!")
+    permutation = "oaeiu"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Giidbya my giid Lird!")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
+    
+    message = SubMessage("Why are you not eating correctly?")
+    permutation = "oaeiu"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Why ora yiu nit aoteng cirractly?")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
